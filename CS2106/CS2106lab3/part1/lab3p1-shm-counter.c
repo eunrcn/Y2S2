7e8190390a2f5916e1698806a86ff00346f72d9a
Eunice Koh <eunicekoh46@gmail.com>
2024-03-14 18:16:58 +0800

    raaaaaaaaaaa

--- CS2106/CS2106lab3/part1/lab3p1-shm-counter.c
+++ CS2106/CS2106lab3/part1/lab3p1-shm-counter.c
@@ -1,65 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
 #define NUM_CHILDREN 5
-#define SHM_KEY 1234
 
 int main() {
-  int shmid;
-  int *counter;
 
-  // Create shared memory segment
-  shmid = shmget(SHM_KEY, sizeof(int), IPC_CREAT | 0666);
-  if (shmid == -1) {
-    perror("shmget");
-    exit(EXIT_FAILURE);
-  }
-
-  // Attach shared memory segment
-  counter = (int *)shmat(shmid, NULL, 0);
-  if (counter == (int *)(-1)) {
-    perror("shmat");
-    exit(EXIT_FAILURE);
-  }
-
-  *counter = 0;
-
+  int counter = 0, i;
   pid_t pid;
 
-  for (int i = 0; i < NUM_CHILDREN; i++) {
+  for (i = 0; i < NUM_CHILDREN; i++) {
     pid = fork();
     if (pid == 0)
       break;
   }
-
   if (pid < 0) {
     perror("fork");
     exit(EXIT_FAILURE);
   } else if (pid == 0) {
     // Child process
     printf("Child %d starts\n", i + 1);
-
     // Simulate some work
     for (int j = 0; j < 5; j++) {
-      (*counter)++;
-      printf("Child %d increment counter %d\n", i + 1, *counter);
+      counter++;
+      printf("Child %d increment counter %d\n", i + 1, counter);
       fflush(stdout);
       usleep(250000);
     }
-
-    printf("Child %d finishes with counter %d\n", i + 1, *counter);
-
-    // Detach shared memory segment
-    if (shmdt(counter) == -1) {
-      perror("shmdt");
-      exit(EXIT_FAILURE);
-    }
-
+    printf("Child %d finishes with counter %d\n", i + 1, counter);
     exit(EXIT_SUCCESS);
   }
 
@@ -68,14 +38,8 @@ int main() {
     wait(NULL);
   }
 
-  // Print the final value of the counter
-  printf("Final counter value: %d\n", *counter);
-
-  // Remove shared memory segment
-  if (shmctl(shmid, IPC_RMID, NULL) == -1) {
-    perror("shmctl");
-    exit(EXIT_FAILURE);
-  }
+  // Print the final value of thecounter
+  printf("Final counter value: %d\n", counter);
 
   return 0;
 }
--- CS2106/CS2106lab3/part2/barrier.c
+++ CS2106/CS2106lab3/part2/barrier.c
@@ -1,18 +1,37 @@
+#include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-void init_barrier(int numproc) {
+static int count = 0;
+static int nproc = 0;
+
+void signal_handler(int signum) {
+  // This handler does nothing; it's just used to interrupt the wait call
+}
+
+void init_barrier(int num_proc) {
+  nproc = num_proc;
+  count = 0;
+  signal(SIGUSR1, signal_handler); // Set up signal handler
 }
 
 void reach_barrier() {
+  count++;
+  if (count == nproc) {
+    // If this is the last process, send signal to release the barrier
+    for (int i = 0; i < nproc - 1; i++)
+      kill(getpid(), SIGUSR1);
+  } else {
+    // If not the last process, wait until released by a signal
+    pause();
+    // Send signal to free the next process
+    kill(getpid(), SIGUSR1);
+  }
 }
 
 void destroy_barrier(int my_pid) {
-    if(my_pid != 0) {
-        // Destroy the semaphores and detach
-        // and free any shared memory. Notice
-        // that we explicity check that it is
-        // the parent doing it.
-    }
+  if (my_pid == 0) {
+    // Parent process: Nothing to clean up
+  }
 }
-
-
